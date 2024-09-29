6f202c5cbe3f18dd3c8ff9dfa877f3a6a04e545d
Subhasri K <124023729+subhasri0206@users.noreply.github.com>
2024-09-29 16:54:38 +0530

    updated vertacx

--- Vertacx/assets/js/lightmode.js
+++ Vertacx/assets/js/lightmode.js
@@ -1,19 +1,15 @@
-let lightmode=localStorage.getItem('lightmode')
-const themeSwitch=document.getElementById('theme-switch')
-
-const enableLightmode = () => {
-    document.body.classList.add('.lightmode')
-    localStorage.setItem('lightmode','active')
-}
-
-const disableLightmode = () => {
-    document.body.classList.add('.lightmode')
-    localStorage.setItem('lightmode','null')
-}
-
-if(lightmode=="active") enableLightmode()
-
-themeSwitch.addEventListener("click", () => {
-    lightmode=localStorage.getItem('lightmode')
-    lightmode !== "active" ? enableLightmode() : disableLightmode()
-})
+// Add an event listener to the theme switch button
+document.getElementById("theme-switch").addEventListener("click", function() {
+    const logo = document.getElementById("logo");
+    // Check if the body has the 'darkmode' class
+    if (document.body.classList.contains("darkmode")) {
+        // If it's in dark mode, remove 'darkmode' and add 'lightmode'
+        document.body.classList.remove("darkmode");
+        document.body.classList.add("lightmode");
+        logo.src="../images/vertacx-logo-light.png";
+    } else {
+        // If it's in light mode, switch back to dark mode
+        document.body.classList.remove("lightmode");
+        document.body.classList.add("darkmode");
+    }
+});
