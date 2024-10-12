d0ef3a0b77e8539badb5b18ef534bfd6b632dc7f
Subhasri K <124023729+subhasri0206@users.noreply.github.com>
2024-10-12 16:07:55 +0530

    updated rock paper scissor game

---
+++ Rock-Paper-Scissor/assets/js/main.js
@@ -0,0 +1,121 @@
+
+setTimeout(() => {
+    document.body.classList.remove("preload");
+  }, 500);
+  
+  const btnRules = document.querySelector(".rules-btn");
+  const btnClose = document.querySelector(".close-btn");
+  const modalRules = document.querySelector(".modal");
+  
+  const CHOICES = [
+    {
+      name: "paper",
+      beats: "rock",
+    },
+    {
+      name: "scissors",
+      beats: "paper",
+    },
+    {
+      name: "rock",
+      beats: "scissors",
+    },
+  ];
+  const choiceButtons = document.querySelectorAll(".choice-btn");
+  const gameDiv = document.querySelector(".game");
+  const resultsDiv = document.querySelector(".results");
+  const resultDivs = document.querySelectorAll(".results-result");
+  
+  const resultWinner = document.querySelector(".results-winner");
+  const resultText = document.querySelector(".results-text");
+  
+  const playAgainBtn = document.querySelector(".play-again");
+  
+  const scoreNumber = document.querySelector(".score-number");
+  let score = 0;
+  
+  // Game Logic
+  choiceButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const choiceName = button.dataset.choice;
+      const choice = CHOICES.find((choice) => choice.name === choiceName);
+      choose(choice);
+    });
+  });
+  
+  function choose(choice) {
+    const aichoice = aiChoose();
+    displayResults([choice, aichoice]);
+    displayWinner([choice, aichoice]);
+  }
+  
+  function aiChoose() {
+    const rand = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[rand];
+  }
+  
+  function displayResults(results) {
+    resultDivs.forEach((resultDiv, idx) => {
+      setTimeout(() => {
+        resultDiv.innerHTML = `
+          <div class="choice ${results[idx].name}">
+            <img src="assets/images/icon-${results[idx].name}.png" alt="${results[idx].name}" />
+          </div>
+        `;
+      }, idx * 1000);
+    });
+  
+    gameDiv.classList.toggle("hidden");
+    resultsDiv.classList.toggle("hidden");
+  }
+  
+  function displayWinner(results) {
+    setTimeout(() => {
+      const userWins = isWinner(results);
+      const aiWins = isWinner(results.reverse());
+  
+      if (userWins) {
+        resultText.innerText = "YOU WIN";
+        resultDivs[0].classList.toggle("winner");
+        keepScore(1);
+      } else if (aiWins) {
+        resultText.innerText = "YOU LOSE";
+        resultDivs[1].classList.toggle("winner");
+        keepScore(-1);
+      } else {
+        resultText.innerText = "DRAW";
+      }
+      resultWinner.classList.toggle("hidden");
+      resultsDiv.classList.toggle("show-winner");
+    }, 1000);
+  }
+  
+  function isWinner(results) {
+    return results[0].beats === results[1].name;
+  }
+  
+  function keepScore(point) {
+    score += point;
+    scoreNumber.innerText = score;
+  }
+
+  playAgainBtn.addEventListener("click", () => {
+    gameDiv.classList.toggle("hidden");
+    resultsDiv.classList.toggle("hidden");
+  
+    resultDivs.forEach((resultDiv) => {
+      resultDiv.innerHTML = "";
+      resultDiv.classList.remove("winner");
+    });
+  
+    resultText.innerText = "";
+    resultWinner.classList.toggle("hidden");
+    resultsDiv.classList.toggle("show-winner");
+  });
+  
+  btnRules.addEventListener("click", () => {
+    modalRules.classList.toggle("show-modal");
+  });
+  btnClose.addEventListener("click", () => {
+    modalRules.classList.toggle("show-modal");
+  });
